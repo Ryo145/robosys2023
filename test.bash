41ba84e3c1995f0ac31b40cb518afbd5aed7eb71
Ryosuke <ryo135791113@gmail.com>
2023-10-26 12:36:13 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -2,7 +2,15 @@
 # SPDX-FileCopyrightText; 2023 Ryosuke Suxuki ryo135791113@gmail.com
 # SPDX-License-Identifier: BSD-3-Clause
 
+ng (){
+echo NG at Line $1
+res=1
+}
+
+res=0
+
 out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
 
-
-[ "${out}" = 15 ]
+[ "$res" = 0 ] && echo OK
+exit $res

81991a208545f195da65f63ee40418eeb023ca71
Ryosuke <ryo135791113@gmail.com>
2023-11-29 10:03:49 +0900

    fix

--- test1.bash
+++ test1.bash
@@ -10,43 +10,43 @@ ng() {
 res=0
 
 out=$(python3 vbn 0 0)
-expected=$'加算の結果:  0.0\n減算の結果:  0.0\n乗算の結果:  0.0\nエラー: 0で割ることはできません'
+expected=$'0.0\n減算の結果:  0.0\n乗算の結果:  0.0\nエラー: 0で割ることはできません'
 if [ "${out}" != "${expected}" ]; then
     ng ${LINENO}
 fi
 
 out=$(python3 vbn 1 2)
-expected=$'加算の結果:  3.0\n減算の結果:  -1.0\n乗算の結果:  2.0\n除算の結果:  0.5'
+expected=$'3.0\n減算の結果:  -1.0\n乗算の結果:  2.0\n除算の結果:  0.5'
 if [ "${out}" != "${expected}" ]; then
     ng ${LINENO}
 fi
 
 out=$(python3 vbn 10 0)
-expected=$'加算の結果:  10.0\n減算の結果:  10.0\n乗算の結果:  0.0\nエラー: 0で割ることはできません'
+expected=$'10.0\n減算の結果:  10.0\n乗算の結果:  0.0\nエラー: 0で割ることはできません'
 if [ "${out}" != "${expected}" ]; then
     ng ${LINENO}
 fi
 
 out=$(python3 vbn -1 -2)
-expected=$'加算の結果:  -3.0\n減算の結果:  1.0\n乗算の結果:  2.0\n除算の結果:  0.5'
+expected=$'-3.0\n減算の結果:  1.0\n乗算の結果:  2.0\n除算の結果:  0.5'
 if [ "${out}" != "${expected}" ]; then
     ng ${LINENO}
 fi
 
 out=$(python3 vbn -1 0)
-expected=$'加算の結果:  -1.0\n減算の結果:  -1.0\n乗算の結果:  -0.0\nエラー: 0で割ることはできません'
+expected=$'-1.0\n減算の結果:  -1.0\n乗算の結果:  -0.0\nエラー: 0で割ることはできません'
 if [ "${out}" != "${expected}" ]; then
     ng ${LINENO}
 fi
 
 out=$(python3 vbn -1.2 2)
-expected=$'加算の結果:  0.8\n減算の結果:  -3.2\n乗算の結果:  -2.4\n除算の結果:  -0.6'
+expected=$'0.8\n減算の結果:  -3.2\n乗算の結果:  -2.4\n除算の結果:  -0.6'
 if [ "${out}" != "${expected}" ]; then
     ng ${LINENO}
 fi
 
 out=$(python3 vbn 2 3 4)
-expected=$'加算の結果:  9.0\n減算の結果:  -5.0\n乗算の結果:  24.0\n除算の結果:  0.16666666666666666'
+expected=$'9.0\n減算の結果:  -5.0\n乗算の結果:  24.0\n除算の結果:  0.16666666666666666'
 if [ "${out}" != "${expected}" ]; then
     ng ${LINENO}
 fi
